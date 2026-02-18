ec5b13c0bcc74dc6cac70d06dc911e73bae46697
Laysar <laysar.main@gmail.com>
2026-02-18 10:13:08 -0500

    Added monokai theme

--- .config/nvim/lua/plugins/MonokaiPro.lua
+++ .config/nvim/lua/plugins/MonokaiPro.lua
@@ -7,11 +7,12 @@ return {
 			styles = {
 				comments = { italic = false }, -- Disable italics in comments
 			},
+			filter = "ristretto",
 		})
 
 		-- Load the colorscheme here.
 		-- Like many other themes, this one has different styles, and you could load
 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("monokai-pro-ristretto")
+		vim.cmd.colorscheme("monokai-pro")
 	end,
 }
