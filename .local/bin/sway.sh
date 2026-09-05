491af6ea06e0c8e4bdc1f38d19cc1e0dad19d4df
Laysar <laysar.main@gmail.com>
2026-09-04 23:20:23 -0400

    Sway: added PROTON_ENABLE_WAYLAND env var on startup

--- .local/bin/sway.sh
+++ .local/bin/sway.sh
@@ -9,6 +9,7 @@ export XDG_DATA_HOME=$HOME/.local/share
 export GTK_THEME=Adwaita:dark
 export GTK2_RC_FILES=/usr/share/themes/Adwaita-dark/gtk-2.0/gtkrc
 export MOZ_ENABLE_WAYLAND=1
+export PROTON_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
 export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_WAYLAND_FORCE_DPI=physical
